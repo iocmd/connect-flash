72610961585458da236512a2ebed1b7eba0d42db
coderiaser <coderaiser@cloudcmd.io>
2026-05-25 13:13:56 +0300

    feature: connect-flash: add

---
+++ .madrun.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const {
+    run,
+    series,
+    parallel,
+} = require('madrun');
+
+module.exports = {
+    'test': () => 'NODE_PATH=lib node_modules/.bin/vows test/*-test.js',
+    'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
+    'lint': () => `putout .`,
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
+    'fix:lint': () => run('lint', '--fix'),
+    'coverage': async () => `c8 ${await run('test')}`,
+    'report': () => 'c8 report --reporter=lcov',
+};
